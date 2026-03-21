f0967087aeb87390d8e76ce5e0587067ee534a80
dimitris_dlt <156653066+dimitrisDlt@users.noreply.github.com>
2026-03-21 20:03:02 +0200

    Add files via upload

--- sources/lim_list.c
+++ sources/lim_list.c
@@ -1,4 +1,5 @@
 #include "lim_list.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,17 +14,26 @@ struct List createList() {
     return newList;
 }
 
-void appendList(struct List* list, Type value) {
+bool appendInList(struct List* list, Type value) {
 
     if(list->head == NULL) {
 
         printf("Creating list head..\n");
 
-        list->head = malloc(sizeof(IntNode));
+        list->head = malloc(sizeof(Node));
+
+        if(list->head == NULL) {
+
+            printf("Failed to malloc list head. Function will return false.\n");
+            return false;
+        }
+
         list->head->value = value;
         list->head->next = NULL;
         list->size = 1;
         list->tail = list->head;
+
+        return true;
     }
     else {
 
@@ -31,33 +41,128 @@ void appendList(struct List* list, Type value) {
 
             printf("Creating list second element..\n");
 
-            list->head->next = malloc(sizeof(IntNode));
+            list->head->next = malloc(sizeof(Node));
+
+            if(list->head->next == NULL) {
+
+                printf("Failed to malloc list second element. Function will return false.\n");
+                return false;
+            }
+
             list->head->next->value = value;
             list->head->next->previous = list->head;
             list->head->next->next = NULL;
             list->size++;
             list->tail = list->head->next;
+
+            return true;
         }
         else {
 
             printf("Creating new list element..\n");
 
-            IntNode* currentNode = list->head->next;
+            Node* currentNode = list->head->next;
 
             while(currentNode->next) {
 
                 currentNode = currentNode->next;
             }
 
-            currentNode->next = malloc(sizeof(IntNode));
+            currentNode->next = malloc(sizeof(Node));
+
+            if(currentNode->next == NULL) {
+
+                printf("Failed to malloc list new element. Function will return false.\n");
+                return false;
+            }
+
             currentNode->next->value = value;
             currentNode->next->previous = currentNode;
             currentNode->next->next = NULL;
             list->tail = currentNode->next;
             list->size++;
+
+            return true;
         }
     }
 }
+
+bool insertInList(List* list, int index, Type value) {
+
+    if(index > list->size) {
+
+        printf("Index requested for insertion exceeds list size! Function will return false.\n");
+        return false;
+    }
+
+    if(index == list->size) {
+        appendInList(list, value);
+        return true;
+    }
+
+    Node* newNode = (Node*) malloc(sizeof(Node));
+
+    if(newNode == NULL) {
+
+        printf("Insertion failed due to corrupt malloc() command! Function will exit without insertion.\n");
+        return false;
+    }
+
+    newNode->value = value;
+
+    int isIndexNearEnd = (int) (index > list->size / 2);
+
+    switch (isIndexNearEnd) {
+
+        case false: {
+
+            Node* currentNode = list->head;
+
+            int count = 0;
+
+            while(currentNode->next && count < index) {
+
+                currentNode = currentNode->next;
+                count++;
+            }
+
+            newNode->previous = currentNode->previous;
+            currentNode->previous->next = newNode;
+
+            newNode->next = currentNode;
+            currentNode->previous = newNode;
+
+            list->size++;
+
+            return true;
+
+        }
+
+        case true: default: {
+
+            Node* currentNode = list->tail;
+
+            int count = list->size - 1;
+
+            while(currentNode->previous && count > index) {
+
+                currentNode = currentNode->previous;
+                count--;
+            }
+
+            newNode->previous = currentNode->previous;
+            currentNode->previous->next = newNode;
+
+            newNode->next = currentNode;
+            currentNode->previous = newNode;
+
+            list->size++;
+
+            return true;
+        }
+    }
+}
+
 
 Type atListIndex(List* list, int index) {
 
@@ -71,7 +176,7 @@ Type atListIndex(List* list, int index) {
         return 0;
     }
 
-    IntNode* currentNode = list->head;
+    Node* currentNode = list->head;
 
     if(list->head == NULL) {
 
@@ -119,7 +224,7 @@ void freeList(List* list) {
         return;
     }
 
-    IntNode* currentNode = list->head;
+    Node* currentNode = list->head;
 
     while(currentNode->next) {
 
